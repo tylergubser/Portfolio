c989a2a4851cf50f301bbac45706a43d3d4c394f
tylergubser <tylergubser2200@gmail.com>
2022-05-16 10:06:52 -0500

    added youtube link

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,3 +1,4 @@
+import React from 'react';
 
 
 function Projects() {
@@ -15,7 +16,11 @@ function Projects() {
                     <p>
                     <a className="hover:text-red-400" href="https://github.com/tylergubser/Drink-up-backend-2">Backend GitHub Repository</a>
                     </p>
+                    <p>
+                    <a className="hover:text-red-400" href="https://www.youtube.com/watch?v=xy7lcuOWRXQ">Demo Video</a>
+                    </p>
                 </div>
+              
             </div>
             <div className="text-center">
                 <h1 className="text-white text-center underline text-2xl font-bold italic">East Coast Fishing Co.</h1>
