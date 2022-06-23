5b84eeac1a05dfdf66e4828271216499c7a34a3d
tylergubser <tylergubser2200@gmail.com>
2022-06-23 10:59:38 -0500

    update

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -15,37 +15,37 @@ function Projects() {
                     <h1 className="text-white text-center underline text-2xl font-bold italic object-center">Drink Up</h1>
                     <img src={drink} alt="drink" className="justify-center" />
                     <p>
-                    <a className="hover:text-red-400" href="https://github.com/tylergubser/drink-up/tree/main/drink-up">Frontend GitHub Repository</a>
+                    <a className="text-white hover:text-red-400" href="https://github.com/tylergubser/drink-up/tree/main/drink-up">Frontend GitHub Repository</a>
                     </p>
                     <p>
-                    <a className="hover:text-red-400" href="https://github.com/tylergubser/Drink-up-backend-2">Backend GitHub Repository</a>
+                    <a className="text-white hover:text-red-400" href="https://github.com/tylergubser/Drink-up-backend-2">Backend GitHub Repository</a>
                     </p>
                     <p>
-                    <a className="hover:text-red-400" href="https://www.youtube.com/watch?v=xy7lcuOWRXQ">Demo Video</a>
+                    <a className="text-white hover:text-red-400" href="https://www.youtube.com/watch?v=xy7lcuOWRXQ">Demo Video</a>
                     </p>
                 </div>
                 <div className="text-center object-center w-96 h-80 mx-auto">
                     <h1 className="text-white text-center underline text-2xl font-bold italic">East Coast Fishing Co.</h1>
                     <img src={fish} alt="fish"/>
                     <p>
-                    <a className="hover:text-red-400" href="https://github.com/tylergubser/East-Coast-Fishing">GitHub Repository</a>
+                    <a className="text-white hover:text-red-400" href="https://github.com/tylergubser/East-Coast-Fishing">GitHub Repository</a>
                     </p>
                 </div>
                 <div className="text-center w-96 h-80 mx-auto">
                     <h1 className="text-white text-center underline text-2xl font-bold italic">Rick and Morty Fan Page</h1>
                     <img src={rickmorty} alt="rick and morty" className="justify-center" />
                     <p>
-                    <a className="hover:text-red-400" href="https://github.com/tylergubser/phase1RickAndMortyProject">GitHub Repository</a>
+                    <a className="text-white hover:text-red-400" href="https://github.com/tylergubser/phase1RickAndMortyProject">GitHub Repository</a>
                     </p>
                     <p>
-                    <a className="hover:text-red-400" href="https://tylergubser.github.io/phase1RickAndMortyProject/">Website</a>
+                    <a className="text-white hover:text-red-400" href="https://tylergubser.github.io/phase1RickAndMortyProject/">Website</a>
                     </p>
                 </div>
                 <div className="text-center w-96 h-80 mx-auto">
                     <h1 className="text-white text-center underline text-2xl font-bold italic">Portfolio Game</h1>
                     <img src={game} alt="portfolio game" className="justify-center" />
                     <p>
-                    <a className="hover:text-red-400" href="https://github.com/tylergubser/portfolio-game-server">GitHub Repository</a>
+                    <a className="text-white hover:text-red-400" href="https://github.com/tylergubser/portfolio-game-server">GitHub Repository</a>
                     </p>
                     {/* <p>
                     <a className="hover:text-red-400" href="https://tylergubser.github.io/phase1RickAndMortyProject/">Website</a>
